a27ff5f83204c861ad46bd3ba2bf15bf47397172
JacobAronowitz <jacobaronowitzmail@gmail.com>
2024-04-05 11:54:18 -0400

    Added scroll function to bypass interception and refreshed some elements, it works now on my end

--- src/test/java/Meals.java
+++ src/test/java/Meals.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class Meals extends Setup{
+public class Meals extends Setup {
 
     @Test(priority = 1)
     void RestaurantSelection() {
@@ -25,16 +25,15 @@ public class Meals extends Setup{
 
     @Test(priority = 2)
     void StandardMealSection() {
-        //driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/main/div[5]/div/div[2]/div[1]/div[2]/div/div/nav/div[3]/button")).click(); //Navigates to the premade bowls
-        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/main/div[4]/div/div[2]/div[1]/div[2]/div/div/nav/div[2]/button")).click();
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/main/div[4]/div/div[2]/div[1]/div[2]/div/div/nav/div[3]/button")).click(); //Navigates to the premade bowls
         wait(2000);
-        Assert.assertEquals(driver.getCurrentUrl(),"https://www.ubereats.com/store/chipotle-mexican-grill-10010-university-plaza-drive/gV8nhcqQThWJqxe97nKKQQ?diningMode=DELIVERY&sc=SEARCH_SUGGESTION"); //Checks to verify URL
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.ubereats.com/store/chipotle-mexican-grill-10010-university-plaza-drive/gV8nhcqQThWJqxe97nKKQQ?diningMode=DELIVERY&sc=SEARCH_SUGGESTION"); //Checks to verify URL
         wait(4000);
     }
 
     @Test(priority = 3)
     void OrderStandardMeal() {
-        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/main/div[5]/div/div[3]/div/ul/li[3]/div/ul/li[3]/a/div/div[2]/div/button")).click(); //Selects premade bowl
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/main/div[4]/div/div[3]/div/ul/li[3]/div/ul/li[3]/a/div/div[2]/div/button")).click(); //Selects premade bowl
         wait(4000);
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[2]/div[3]/div/button[1]")).click(); //Adds to cart
         wait(4000);
@@ -42,9 +41,12 @@ public class Meals extends Setup{
 
     @Test(priority = 4)
     void OrderCustomMeal() {
-        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/main/div[5]/div/div[2]/div[1]/div[2]/div/div/nav/div[2]/button")).click(); //Navigates to custom items
-        wait(2000);
-        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/main/div[5]/div/div[3]/div/ul/li[2]/div/ul/li[1]/a/div/div[2]/div/button")).click(); //selects custom bowl
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/main/div[4]/div/div[2]/div[1]/div[2]/div/div/nav/div[3]/button")).click(); //Navigates to custom items
+        wait(3000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,-600)", ""); //Scrolls up by 200 pixels
+        wait(3000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/main/div[4]/div/div[3]/div/ul/li[2]/div/ul/li[1]/a/div/div[2]/div/button")).click(); //selects custom bowl
         wait(3000);
         driver.findElement(By.id("quick_view-item-2a5005c4-e44d-5918-a05a-06b4669357e4-1-742f32d2-5332-53c8-b038-f529dfc31cb5+0")).click(); //Selects sofritas
         wait(3000);
